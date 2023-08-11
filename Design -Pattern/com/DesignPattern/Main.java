32b4693131ef67edbae0f63918c72ba546422729
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2023-08-11 12:30:48 +0500

    Update Main.java

--- Design -Pattern/com/DesignPattern/Main.java
+++ Design -Pattern/com/DesignPattern/Main.java
@@ -4,15 +4,23 @@ import com.DesignPattern.Adapter.*;
 
 public class Main {
     public static void main(String[] args) {
+        // _-------------------------------_ Adapter Pattern
         Drone drone = new SuperDrone();
 
         //adapting the adapter
         Duck adapter = new Adapter(drone);
         test(adapter);
+
+        // _------------------------------_ Observer Design Pattern
+         WeatherStation sub = new WeatherStation();
+        sub.subscribe(new AlertSystem());
+        sub.subscribe(new UserInterface());
+        sub.subscribe(new Logger());
+        sub.change(27,56,92);
+
     }
     public static void test(Duck duck){
         duck.quack();
         duck.fly();
-
     }
 }
